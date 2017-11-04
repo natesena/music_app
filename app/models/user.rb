ddb07329f08733753d88ceb5e5e9ca8895b371a0
Nathaniel Sena <nate.sena1@gmail.com>
2017-11-03 18:21:41 -0700

    committing anything that may have changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ApplicationRecord
     
     has_attached_file :avatar,
     styles: {medium: "300x300#", thumb: "100x100#"},
-    :default_url => ActionController::Base.helpers.asset_path("missing.jpg")
+    # :default_url => ActionController::Base.helpers.asset_path("missing.jpg")
+    :default_url => '/images/missing.jpg'
 
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
     has_attached_file :covimg,
     styles: {user_cov: "800x300#"},
-    :default_url => ActionController::Base.helpers.asset_path("missing_cover_photo.jpg")
+    # :default_url => ActionController::Base.helpers.asset_path("missing_cover_photo.jpg")
+    :default_url => '/images/missing_cover_photo.jpg'
 
     validates_attachment_content_type :covimg, content_type: /\Aimage\/.*\z/
 end
